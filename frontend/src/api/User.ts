e3b93ec51b0087a53b61554722cefb390f271d17
moxort <99677018+moxort@users.noreply.github.com>
2024-08-09 18:26:41 +0300

    feat(api): Added http handling and Axious (#5)
    feat(api): Added http handling and Axious (#5)

---
+++ frontend/src/api/User.ts
@@ -0,0 +1,12 @@
+import API from './API.ts';
+
+export const fetchUser = async (userId: string) => {
+  try {
+      //TODO possibly change this this to another route
+    const response = await API.get(`/users/${userId}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching user:', error);
+    throw error;
+  }
+};
